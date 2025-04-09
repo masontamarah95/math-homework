d42e0efbd4ba803c71c2fb033fc85d305ef58594
masontamarah95 <masontamarah95@gmail.com>
2025-04-09 13:04:26 +0000

    Add 31.rs

---
+++ src/31.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    let y = 10;
+    let z = 20;
+
+    println!("The sum of {} and {} is {}", x, y, x + y);
+    println!("The product of {} and {} is {}", x, y, x * y);
+
+    // Example: This code will print "Hello, World!" to the console
+}
