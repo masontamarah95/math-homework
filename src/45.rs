bf926fdc2ac34b22ad2ad80badb2e341e9e68bb8
masontamarah95 <masontamarah95@gmail.com>
2025-05-24 06:33:11 +0000

    Add 45.rs

---
+++ src/45.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Define variables
+    let x = 5;
+    let y = 3;
+
+    // Perform arithmetic operations
+    let result = x + y;
+    println!("The sum of {} and {} is {}", x, y, result);
+}
